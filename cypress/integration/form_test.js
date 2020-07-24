92c417f60688e033c95e840ef7667bf4a6131967
Lynda Santiago <you@example.com>
2020-07-24 13:06:08 -0400

    ran cypress test passed successfully

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,26 @@
+
+
+describe('Testing our form', ()=>{
+    beforeEach(function(){
+        //runs before each test in this block
+        cy.visit(' http://localhost:3000/pizza')
+    })
+    it('Testing inputs and that form submits', () => {
+        //Arrange - Get the element
+     
+        cy.get('#name')
+        //Act - Mimic User Interaction
+        .type('Lynda Santiago')
+        //Assert - Test / Verify
+        .should('have.value', 'Lynda Santiago')
+      //Testing that you can select multiple toppings
+      cy.get('#size').select('Small')
+      cy.get('input[type="checkbox"]').check().should('be.checked')
+      cy.get('textarea').type('Ready to submit form')
+      cy.get('.btnOrder').click()
+     
+
+     
+    })
+  
+})
